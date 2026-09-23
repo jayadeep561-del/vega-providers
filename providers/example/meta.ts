8ea24dea6c37dd5d4820e431854d8ee6cb9ac3d2
jayadeep561-del <jayadeep561@gmail.com>
2026-09-23 18:07:16 +0530

    Update meta.ts

--- providers/example/meta.ts
+++ providers/example/meta.ts
@@ -1,33 +1,30 @@
 import { Info, ProviderContext } from "../types";
-
 export const getMeta = async function ({
-  link,
-  providerContext,
+link,
+providerContext,
 }: {
-  link: string;
-  providerContext: ProviderContext;
-}): Promise<Info> {
-  // Fetch and parse metadata for the item using axios and cheerio
-  // const { axios, cheerio } = providerContext;
-  
-  return {
-    title: "Example Content",
-    synopsis: "This is an example synopsis for the Vega App Provider Template. Once you have tested this, delete this example provider and create your own!",
-    image: "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba",
-    imdbId: "tt1234567",
-    type: "movie",
-    linkList: [
-      {
-        title: "Movie Links",
-        directLinks: [
-          {
-            link: "https://example.com/stream-1",
-            title: "Play Movie",
-            type: "movie",
-          },
-        ],
-        quality: "1080p",
-      },
-    ],
-  };
+link: string;
+providerContext: ProviderContext;
+}): Promise<Info> => {
+return {
+title: "Netflix Test Movie",
+synopsis:
+"A test movie used to verify the custom provider and streaming pipeline.",
+image: "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba",
+imdbId: "tt0000000",
+type: "movie",
+linkList: [
+{
+title: "Netflix Test Movie",
+directLinks: [
+{
+link: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+title: "Play Test Movie",
+type: "movie",
+},
+],
+quality: "1080p",
+},
+],
+};
 };
